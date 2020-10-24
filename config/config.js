bf1442f6cffc2926afa797a07c470899c02e297c
golden-state0fmind <areyes2009@yahoo.com>
2020-10-24 09:49:46 -0700

    just changed the username until DB name is concrete

--- config/config.js
+++ config/config.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": "brian",
+    "username": "antonioreyes",
     "password": process.env.DB_PASS,
     "database": "express_auth_development",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "test": {
-    "username": "brian",
+    "username": "antonioreyes",
     "password": process.env.DB_PASS,
     "database": "express_auth_test",
     "host": "127.0.0.1",
