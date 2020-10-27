a30eada4ab34800220107216009b959f4a59bad3
Brian Blankenship <brian.ga.edu@gmail.com>
2020-10-27 16:58:45 -0500

    Added validations to user model.

--- models/user.js
+++ models/user.js
@@ -25,39 +25,120 @@ module.exports = (sequelize, DataTypes) => {
 				type: DataTypes.STRING(25),
 				allowNull: false,
 				unique: true,
+				constraints: false,
+				validate: {
+					len: {
+						args: [5, 25],
+						// eslint-disable-next-line prettier/prettier
+						msg:
+							'Username must be between 5 and 25 characters',
+					},
+					min: {
+						args: 5,
+						msg: 'Username must be at least 5 characters',
+					},
+					max: {
+						args: 25,
+						msg: "Username can't be more than 25 characters.",
+					},
+					isAlphanumeric: true,
+					notEmpty: true,
+					notNull: true,
+				},
 			},
 			password: {
 				type: DataTypes.STRING(99),
 				allowNull: false,
+				validate: {
+					len: {
+						args: [15, 99],
+						msg: 'Must be 15 to 99 characters.',
+					},
+					notContains: ' ',
+					min: {
+						args: 15,
+						msg: 'Must contain at least 15 characters.',
+					},
+					max: {
+						args: 99,
+						msg: 'Must contain at least 99 characters.',
+					},
+					notEmpty: true,
+					notNull: true,
+					isLowercase: true,
+					isUppercase: true,
+				},
 			},
 			firstName: {
 				type: DataTypes.STRING,
 				allowNull: false,
+				validate: {
+					isAlpha: true,
+					notNull: true,
+					notEmpty: true,
+				},
 			},
 			lastName: {
 				type: DataTypes.STRING,
 				allowNull: false,
+				validate: {
+					isAlpha: true,
+					notNull: true,
+					notEmpty: true,
+				},
 			},
 			email: {
 				type: DataTypes.STRING,
 				allowNull: false,
 				unique: true,
+				validate: {
+					notNull: true,
+					notEmpty: true,
+					isEmail: true,
+				},
 			},
 			title: {
 				type: DataTypes.STRING(25),
 				allowNull: true,
+				validate: {
+					max: {
+						args: 25,
+						msg: 'Can only be between 0 and 25 characters.',
+					},
+				},
 			},
 			quote: {
-				type: DataTypes.STRING(200),
+				type: DataTypes.STRING,
 				allowNull: true,
+				validate: {
+					max: {
+						args: 255,
+						msg: 'Can only be 255 characters long.',
+					},
+					isAlphanumeric: true,
+				},
 			},
 			jobTitle: {
 				type: DataTypes.STRING(25),
 				allowNull: true,
+				validate: {
+					max: {
+						args: 25,
+						msg: 'Can only be 25 characters long.',
+					},
+					isAlpha: true,
+				},
 			},
 			bio: {
 				type: DataTypes.STRING(500),
 				allowNull: true,
+				validate: {
+					max: {
+						args: 500,
+						msg: 'Can only be 500 characters long.',
+					},
+					isAlphanumeric: true,
+				},
 			},
 			profilePicture: {
 				type: DataTypes.STRING,
