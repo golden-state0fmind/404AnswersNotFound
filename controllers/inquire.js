ebdbbaf0d34010cff8b023a18135514338beec6d
Brian Blankenship <brian.ga.edu@gmail.com>
2020-10-27 10:41:55 -0500

    Setup route for more views, added view for viewing a post.

--- controllers/inquire.js
+++ controllers/inquire.js
@@ -8,10 +8,21 @@ router.get('/create/inquisition', (req, res) => {
 		title: 'Ask a Question',
 		description: null,
 		// eslint-disable-next-line prettier/prettier
-		style: "/css/inquisition.css",
+		style: '/css/inquisition.css',
 	};
 
 	res.render('inquire/inquisition', { meta: locals });
+});
+
+router.get('/inquiry/:id', (req, res) => {
+	const locals = {
+		title: req.params.id,
+		description: req.body.summary,
+		// eslint-disable-next-line prettier/prettier
+		style: 'css/inquiry.css',
+	};
+
+	res.render('/inquire/inquiry', { meta: locals });
 });
 
 module.exports = router;
