622b6cd13e26e6ced1c1d54bc14500c90dbfc256
Brian Blankenship <brian.ga.edu@gmail.com>
2020-10-29 17:19:23 -0500

    Added a table to track catch errors.

---
+++ models/bug.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model, DatabaseError
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class bug extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  bug.init({
+    error: DataTypes.TEXT,
+    location: DataTypes.STRING,
+    activity: DataTypes.STRING,
+    user: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'bug',
+  });
+  return bug;
+};
