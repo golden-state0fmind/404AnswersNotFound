83da583bad0c3c2dbf3295d2e0095b907cfa8ec9
golden-state0fmind <areyes2009@yahoo.com>
2020-10-29 21:53:28 -0700

    wiped this file clean since it wont be used

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,2 @@
-/* eslint-disable no-constant-condition */
 /* eslint-disable prettier/prettier */
-const handleClick = e => {
-     if (e.target.id === 'registerBtn') {
-          window.location.href = '/auth/signup';
-     } else if (e.target.id === 'spanish_inquisition') {
-          window.location.href = '/inquire/create/inquisition';
-     }
-}
-document.getElementById('registerBtn').addEventListener('click', handleClick);
-document.getElementById('spanish_inquisition').addEventListener('click', handleClick);
+
